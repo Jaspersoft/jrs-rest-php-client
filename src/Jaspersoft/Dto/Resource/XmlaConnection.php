59a741799b0470e931bf72be60be981797c6fcee
Grant Bacon <btnarg@gmail.com>
2013-09-12 19:02:23 -0500

    Fix bug 34322 - changed xmlaDataSource attribute name to dataSource to match API's expected name

--- src/Jaspersoft/Dto/Resource/XmlaConnection.php
+++ src/Jaspersoft/Dto/Resource/XmlaConnection.php
@@ -5,7 +5,7 @@ namespace Jaspersoft\Dto\Resource;
 class XmlaConnection extends Resource
 {
     public $url;
-    public $xmlaDataSource;
+    public $dataSource;
     public $catalog;
     public $username;
     public $password;

d842f68bc9a54d46716156ec35fbb9acd15c5d31
Grant Bacon <btnarg@gmail.com>
2013-09-14 17:36:04 -0500

    Created a function to add paramters to report options

--- src/Jaspersoft/Dto/Resource/ReportOptions.php
+++ src/Jaspersoft/Dto/Resource/ReportOptions.php
@@ -6,4 +6,14 @@ class ReportOptions extends Resource
 {
     public $reportUri;
     public $reportParameters;
+
+    /** Add a parameter to the report option
+     *
+     * @param $name string the name of the parameter
+     * @param $value array an array of the selected values for the parameter
+     */
+    public function addParameter($name, $value) {
+        $this->reportParameters[] = array("name" => $name, "value" => $value);
+    }
+
 }

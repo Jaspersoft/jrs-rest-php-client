e0d7c9c6aa6f178b30a7fad09384518d299ae563
Grant Bacon <btnarg@gmail.com>
2013-10-04 16:59:08 -0500

    Added additional functionality to runReport service (freshData, saveDataSnapshot, pages, and transformerKey

--- src/Jaspersoft/Service/ReportService.php
+++ src/Jaspersoft/Service/ReportService.php
@@ -25,12 +25,16 @@ class ReportService
 	 * @param string $page - Request a specific page
 	 * @param string $attachmentsPrefix - a URI to prefix all image attachment sources with (must include trailing slash if needed)
 	 * @param array $inputControls - associative array of key => value for any input controls
+     * @param boolean $interactive Should reports using Highcharts be interactive?
+     * @param boolean $onePagePerSheet Produce paginated XLS or XLSX?
+     * @param string $pages Return a set of continous pages (example "5-10")
+     * @param string $transformerKey For use when running a report as a JasperPrint. Specifies print element transformers
 	 * @return string - the binary data of the report to be handled by external functions
 	 */
 	public function runReport($uri, $format = 'pdf', $page = null, $attachmentsPrefix = null, $inputControls = null,
-                                $interactive = true, $onePagePerSheet = false) {
+                                $interactive = true, $onePagePerSheet = false, $freshData = true, $saveDataSnapshot = false, $pages = null, $transformerKey = null) {
 		$url = $this->restUrl2 . '/reports' . $uri . '.' . $format;
-        $url .= '?' . Util::query_suffix(array_merge(compact("page", "attachmentsPrefix", "interactive", "onePagePerSheet"), $inputControls));
+        $url .= '?' . Util::query_suffix(array_merge(compact("page", "attachmentsPrefix", "interactive", "onePagePerSheet", "freshData", "saveDataSnapshot", "pages", "transformerKey"), $inputControls));
 		$binary = $this->service->prepAndSend($url, array(200), 'GET', null, true);
 		return $binary;
 	}

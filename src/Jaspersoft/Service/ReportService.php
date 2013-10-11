1713d756a2d251476be9c8e39059de2c8cceb2af
Grant Bacon <btnarg@gmail.com>
2013-10-11 17:09:36 -0500

    Allow null values to not interfere with ReportService's runReport function

--- src/Jaspersoft/Service/ReportService.php
+++ src/Jaspersoft/Service/ReportService.php
@@ -16,25 +16,30 @@ class ReportService
 		$this->service = $service;
 		$this->restUrl2 = $baseUrl;
 	}
-	
-	/**
-	 * This function runs and retrieves the binary data of a report.
+
+    /**
+     * This function runs and retrieves the binary data of a report.
      *
-	 * @param string $uri - URI for the report you wish to run
-	 * @param string $format - The format you wish to receive the report in (default: pdf)
-	 * @param string $page - Request a specific page
-	 * @param string $attachmentsPrefix - a URI to prefix all image attachment sources with (must include trailing slash if needed)
-	 * @param array $inputControls - associative array of key => value for any input controls
-     * @param boolean $interactive Should reports using Highcharts be interactive?
-     * @param boolean $onePagePerSheet Produce paginated XLS or XLSX?
+     * @param string $uri - URI for the report you wish to run
+     * @param string $format - The format you wish to receive the report in (default: pdf)
+     * @param string $page - Request a specific page
+     * @param string $attachmentsPrefix - a URI to prefix all image attachment sources with (must include trailing slash if needed)
+     * @param array $inputControls - associative array of key => value for any input controls
+     * @param bool $interactive Should reports using Highcharts be interactive?
+     * @param bool $onePagePerSheet Produce paginated XLS or XLSX?
+     * @param bool $freshData
+     * @param bool $saveDataSnapshot
      * @param string $pages Return a set of continous pages (example "5-10")
      * @param string $transformerKey For use when running a report as a JasperPrint. Specifies print element transformers
-	 * @return string - the binary data of the report to be handled by external functions
-	 */
-	public function runReport($uri, $format = 'pdf', $page = null, $attachmentsPrefix = null, $inputControls = array(),
+     * @return string Binary data of report
+     */
+	public function runReport($uri, $format = 'pdf', $page = null, $attachmentsPrefix = null, $inputControls = null,
                                 $interactive = true, $onePagePerSheet = false, $freshData = true, $saveDataSnapshot = false, $pages = null, $transformerKey = null) {
 		$url = $this->restUrl2 . '/reports' . $uri . '.' . $format;
-        $url .= '?' . Util::query_suffix(array_merge(compact("page", "attachmentsPrefix", "interactive", "onePagePerSheet", "freshData", "saveDataSnapshot", "pages", "transformerKey"), $inputControls));
+        if (empty($inputControls))
+            $url .= '?' . Util::query_suffix(compact("page", "attachmentsPrefix", "interactive", "onePagePerSheet", "freshData", "saveDataSnapshot", "pages", "transformerKey"));
+        else
+            $url .= '?' . Util::query_suffix(array_merge(compact("page", "attachmentsPrefix", "interactive", "onePagePerSheet", "freshData", "saveDataSnapshot", "pages", "transformerKey"), $inputControls));
 		$binary = $this->service->prepAndSend($url, array(200), 'GET', null, true);
 		return $binary;
 	}

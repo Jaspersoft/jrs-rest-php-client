f6594c48c695b809a460af4645f0bf066cb26ceb
Grant Bacon <btnarg@gmail.com>
2014-02-20 16:17:24 -0600

    RepositoryService: Update ReportUnit class to handle resources field separately to hide behind the scenes mapping. Fixes #35912

--- src/Jaspersoft/Dto/Resource/ReportUnit.php
+++ src/Jaspersoft/Dto/Resource/ReportUnit.php
@@ -13,5 +13,28 @@ class ReportUnit extends CompositeResource {
     public $jrxml;
     public $inputControls;
     public $files;
+    public $resources;
+
+    public static function createFromJSON($json_data, $type = null) {
+        // Handle resources here as a special case
+        if (!empty($json_data['resources']['resource'])) {
+            $json_data['resources'] = $json_data['resources']['resource'];
+            return parent::createFromJSON($json_data, $type);
+        } else {
+            return parent::createFromJSON($json_data, $type);
+        }
+    }
+
+    public function jsonSerialize() {
+        if (!empty($this->resources)) {
+            $parent = parent::jsonSerialize();
+            $parent_resources = $parent['resources'];
+            unset($parent['resources']);
+            $parent['resources']['resource'] = $parent_resources;
+            return $parent;
+        } else {
+            return parent::jsonSerialize();
+        }
+    }
 
 }
